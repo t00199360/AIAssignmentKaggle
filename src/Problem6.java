95b2961043ad84b806e6f34e4a0224326c749485
t00199360 <t00199360@students.ittralee.ie>
2018-09-26 16:27:11 +0100

    started problem 6, need to finish it once ive worked out the logic

---
+++ src/Problem6.java
@@ -0,0 +1,25 @@
+public class Problem6
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        while (i<1)
+        {
+            int inputs=0;
+            cube();
+            inputs++;
+
+            if (inputs=16)
+            {
+                i++;
+            }
+
+        }
+        System.out.print()
+    }
+
+    private static void cube()
+    {
+
+    }
+}
